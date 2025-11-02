8905badfe91d803fcfdd5e9b50e0b9a00e3e834a
alaminannuar <alaminannuar@gmail.com>
2025-11-02 23:28:12 +0800

    Update CORS settings for deployed frontend

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ const taskRoutes = require("./routes/task");
 const mongoURI =
   process.env.MONGO_URI ||
   "mongodb+srv://testuser1:jsDDkAbGKQMRK3kv@cluster0.cuig9xs.mongodb.net/?appName=Cluster0";
-const frontendURL = process.env.FRONTEND_URL || "http://localhost:5173";
+const frontendURL =
+  process.env.FRONTEND_URL ||
+  "https://task-manager-frontend-jclr.onrender.com/";
 const PORT = process.env.PORT || 5000;
 
 // Connect to MongoDB
@@ -26,7 +28,7 @@ mongoose
     // CORS
     app.use(
       cors({
-        origin: frontendURL,
+        origin:"*",
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
         credentials: true,
       })
